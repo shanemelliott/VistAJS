e01c54488354434fd532326cc153da8bd3bb1e3b
Shane Elliott <67919090+shanemelliott@users.noreply.github.com>
2022-10-07 14:00:27 +0000

    docker work

--- vista/setup.sh
+++ vista/setup.sh
@@ -8,7 +8,7 @@ sudo chown -R 51773:51773 ~/vista
 sudo chmod 775 ~/vista
 #cd ~/vista/dat/vista
 sudo wget -P ~/vista/dat/vista https://foia-vista.worldvista.org/DBA_VistA_FOIA_System_Files/DBA_VISTA_FOIA_2022/DBA_VISTA_FOIA_20220907.zip
-sudo unzip ~/vista/dat/vista/DBA_VISTA_FOIA_20220907.zip
-sudo chmod chmod 775 ~/vista/dat/vista/IRIS.DAT
-docker-compose up 
+sudo unzip ~/vista/dat/vista/DBA_VISTA_FOIA_20220907.zip -d ~/vista/dat/vista
+sudo chmod 775 ~/vista/dat/vista/IRIS.DAT
+docker-compose up -d
 ./xinetd.sh
