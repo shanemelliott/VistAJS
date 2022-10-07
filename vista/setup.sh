b1ad953ff95ce58bed716d23dd434ea6f824dafb
Shane Elliott <67919090+shanemelliott@users.noreply.github.com>
2022-10-07 01:27:18 +0000

    docker work

---
+++ vista/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mkdir ~/vista/
+mkdir ~/vista/merge/
+cp merge.cpf ~/vista/merge/merge.cpf
+mkdir ~/vista/dat
+mkdir ~/vista/dat/vista
+sudo chown -R 51773:51773 ~/vista
+sudo chmod 775 ~/vista
