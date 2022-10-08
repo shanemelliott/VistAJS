6ad9e9a4180010b9d67fe76d8aeb79b3d1e61f81
Shane Elliott <67919090+shanemelliott@users.noreply.github.com>
2022-10-08 15:01:26 +0000

    readme

---
+++ vista/reset.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+read -p "THis will delete the entire VistA setup are you sure? (y/n)" CONT
+if [ "$CONT" = "y" ]; then
+  echo "Deleteing ~/vista directory, run setup.sh to start over";
+  sudo rm -rf ~/vista
+else
+  echo "ok then, not doing anything";
+fi
--- vista/xinetd.sh
+++ vista/xinetd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec -it -u 0 iris /usr/sbin/xinetd -pidfile /run/xinetd.pid -stayalive -inetd_compat
+/usr/sbin/xinetd -pidfile /run/xinetd.pid -stayalive -inetd_compat
