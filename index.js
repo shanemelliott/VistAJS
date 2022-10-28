dcb665c6656d19b1bb3234704111bc2b7e648006
Shane Elliott <67919090+shanemelliott@users.noreply.github.com>
2022-10-28 22:08:07 +0000

    user creation

---
+++ index.js
@@ -0,0 +1,38 @@
+/*jslint node: true */
+/*jshint -W098 */
+'use strict';
+
+var util = require('util');
+var _ = require('underscore');
+var clc = require('cli-color');
+var moment = require('moment');
+var VistaJS = require('./VistaJS');
+const configuration = require('./config');
+var logger = require('bunyan').createLogger({
+    name: 'RpcClient',
+    level: 'info'
+});
+
+function inspect(obj) {
+    return obj ? util.inspect(obj, {
+        depth: null
+    }) : '';
+}
+
+function printResult(error, result) {
+    console.log(clc.red(inspect(error)));
+    console.log(clc.cyan(inspect(result)));
+}
+
+function printJsonResult(error, result) {
+    console.log(clc.red(inspect(error)));
+    var output = result;
+    try {
+        output = JSON.parse(result);
+    } catch (err) {
+        // use default
+    }
+    console.log(clc.cyan(inspect(output)));
+}
+console.log('Executing RPC...')
+VistaJS.callRpc(logger, configuration, 'SDES GET USER PROFILE BY DUZ', [VistaJS.RpcParameter.literal('1')],printJsonResult);
