715bc7822bcbaa601674f0d76cee0de2aa4c5070
Shane Elliott <67919090+shanemelliott@users.noreply.github.com>
2022-10-28 22:16:17 +0000

    added config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    context: 'SDECRPC',
+    host: 'localhost',
+    port: 9093,
+    accessCode: 'VISTAJS123',
+    verifyCode: 'VISTAJS123!!',
+    localIP: '',
+    localAddress: ''
+};
